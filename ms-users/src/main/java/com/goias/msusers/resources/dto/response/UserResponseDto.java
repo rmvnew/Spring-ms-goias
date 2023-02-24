0fb302ce042090d9be03da5464f49f70eddbf2a2
Ricardo <rmvnew@gmail.com>
2023-02-24 11:49:05 -0400

    feat: create UserResponseDto

---
+++ ms-users/src/main/java/com/goias/msusers/resources/dto/response/UserResponseDto.java
@@ -0,0 +1,17 @@
+package com.goias.msusers.resources.dto.response;
+
+import com.goias.msusers.model.Profile;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserResponseDto {
+    private Long userId;
+
+    private String userName;
+
+    private String userEmail;
+
+    private Profile profile;
+}
