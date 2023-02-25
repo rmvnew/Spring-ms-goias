60bf6b044a0f7c534781429ce2c01a396ba70487
Ricardo <rmvnew@gmail.com>
2023-02-25 08:12:56 -0400

    chore: register msusers in discovery

--- ms-users/src/main/java/com/goias/msusers/MsUsersApplication.java
+++ ms-users/src/main/java/com/goias/msusers/MsUsersApplication.java
@@ -2,8 +2,10 @@ package com.goias.msusers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsUsersApplication {
 
 	public static void main(String[] args) {
