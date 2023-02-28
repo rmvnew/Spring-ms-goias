1975676caeea85fd3e404bc652dde1336f3b2bb9
Ricardo <rmvnew@gmail.com>
2023-02-28 13:21:55 -0400

    feat: add recover password

--- ms-users/src/main/java/com/goias/msusers/repository/UserRepository.java
+++ ms-users/src/main/java/com/goias/msusers/repository/UserRepository.java
@@ -1,8 +1,9 @@
 package com.goias.msusers.repository;
 
 import com.goias.msusers.model.User;
-import com.goias.msusers.resources.dto.response.UserResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,4 +13,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserName(String name);
 
     Optional<User> findByUserEmail(String email);
+
+
+    @Query("SELECT u FROM User u WHERE u.userEmail = :email AND u.recoverCode = :code")
+    Optional<User> findByUserEmailAndRecoverCode(@Param("email") String email, @Param("code") String code);
 }
