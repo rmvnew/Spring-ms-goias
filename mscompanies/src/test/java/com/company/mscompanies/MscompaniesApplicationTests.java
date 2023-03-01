33a4f7ea8c94e7093b6c08d0b034b0526ae36a3b
Ricardo <rmvnew@gmail.com>
2023-03-01 16:57:03 -0400

    chore: create mscompanies

---
+++ mscompanies/src/test/java/com/company/mscompanies/MscompaniesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.mscompanies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscompaniesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
